8db9cb809de192d86796e7459bf2c603c9af52dc
Harsh <harshpxv@gmail.com>
2025-05-06 20:58:58 -0400

    [Model] Using the default User class for owner of Journals

--- server/mental_backend/api/models.py
+++ server/mental_backend/api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Journal(models.Model):
     title = models.CharField(max_length=100)
-    owner = models.CharField(max_length=100, blank=True) # I don't wanna make a User rn
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="journals")
     date_created = models.DateField(auto_now_add=True)
 
     def __str__(self):
