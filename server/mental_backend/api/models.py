26735a8505aaec08a76d7768ff7bb5b079c4a7cc
Harsh <harshpxv@gmail.com>
2025-05-06 20:47:03 -0400

    Created a Journal and Note model

--- server/mental_backend/api/models.py
+++ server/mental_backend/api/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
+
+class Journal(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100, blank=True) # I don't wanna make a User rn
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Note(models.Model):
-    note_title = models.CharField(max_length=50)
-    date_created = models.DateField()
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE, related_name="notes")
+    note_title = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
-
-    
+    def __str__(self):
+        return self.note_title
