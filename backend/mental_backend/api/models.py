e4a9f4ad8f51304e5fec4c5cef06f5d01f5d5237
Harsh <harshpxv@gmail.com>
2025-05-09 17:27:47 -0400

    Documenting the models

--- backend/mental_backend/api/models.py
+++ backend/mental_backend/api/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Journal(models.Model):
+    """
+    Journal object that contains all the notes made by a user. The Journal
+    has a title, owner, and the date it was created.
+    """
+
     title = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="journals")
     date_created = models.DateField(auto_now_add=True)
@@ -12,6 +17,11 @@ class Journal(models.Model):
 
 
 class Note(models.Model):
+    """
+    Note object is associated with a journal. It has a title for the note,
+    the date it was created, and the content within the note.
+    """
+
     journal = models.ForeignKey(Journal, on_delete=models.CASCADE, related_name="notes")
     note_title = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now=True)
