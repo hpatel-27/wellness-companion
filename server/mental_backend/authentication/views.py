9917143ffd97337ce6cdaf77e51247eaab06d585
Harsh <harshpxv@gmail.com>
2025-05-09 13:30:53 -0400

    Setting up Register and Login views

--- server/mental_backend/authentication/views.py
+++ server/mental_backend/authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+# Post request to the register 
+class RegisterView(APIView):
+    def post(self, request):
+        pass
+
+
+class LoginView(APIView):
+    def post(self, request):
+        pass
