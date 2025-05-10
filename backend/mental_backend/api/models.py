ccd5c42c8d723dc796c0e666fc138a04decbd2a4
Harsh <harshpxv@gmail.com>
2025-05-10 01:19:52 -0400

    Adding a updated_at field for the journal

--- backend/mental_backend/api/models.py
+++ backend/mental_backend/api/models.py
@@ -11,6 +11,7 @@ class Journal(models.Model):
     title = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="journals")
     date_created = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
