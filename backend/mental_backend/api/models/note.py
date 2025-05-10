5f44d0c19f6f23667642b432c5901eee3a4033ef
Harsh <harshpxv@gmail.com>
2025-05-10 03:11:52 -0400

    Adding some defaults to the mood and sleep score

--- backend/mental_backend/api/models/note.py
+++ backend/mental_backend/api/models/note.py
@@ -20,8 +20,10 @@ class Note(models.Model):
     date_created = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    mood = models.CharField(max_length=50)
-    sleep_score = models.DecimalField(max_digits=2, decimal_places=1)
+    mood = models.CharField(max_length=50, default="Neutral")
+    sleep_score = models.DecimalField(
+        max_digits=2, decimal_places=1, blank=True, null=True
+    )
 
     def __str__(self):
         return self.note_title
