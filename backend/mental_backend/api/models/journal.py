094629eced2a2ed0143af19ae062903efddb86d0
Harsh <harshpxv@gmail.com>
2025-05-10 01:31:16 -0400

    Made a models package to separate the classes

--- backend/mental_backend/api/models/journal.py
+++ backend/mental_backend/api/models/journal.py
@@ -15,18 +15,3 @@ class Journal(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Note(models.Model):
-    """
-    Note object is associated with a journal. It has a title for the note,
-    the date it was created, and the content within the note.
-    """
-
-    journal = models.ForeignKey(Journal, on_delete=models.CASCADE, related_name="notes")
-    note_title = models.CharField(max_length=100)
-    date_created = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.note_title
