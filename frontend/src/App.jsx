29c531c39e11c107e971b91ec418b0cf63dbf945
Harsh <harshpxv@gmail.com>
2025-05-13 20:26:21 -0400

    feat: wrap auth around the app
    AuthProvider wraps around the rest of the app content. As long as we are not on the login or register page, show the NavBar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,28 @@
 import NavBar from "./components/NavBar";
+import { AuthProvider } from "./providers/AuthProvider";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "./styles/index.css";
 
+function AppContent() {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === "/login" || location.pathname === "/register";
+
+  return <>{!isAuthPage && <NavBar />}</>;
+}
+
 function App() {
-  const handleSubmit = () => {
-    console.log("Clicked submit.");
-  };
   return (
-    <>
-      <NavBar />
-      <h1>Wellness Journal</h1>
-      <form action="">
-        <textarea
-          rows={5}
-          cols={30}
-          placeholder="Your thoughts..."
-          name="note"
-          id="note"
-        ></textarea>
-        <button type="submit" onSubmit={handleSubmit}>
-          Save Note
-        </button>
-      </form>
-    </>
+    <AuthProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </AuthProvider>
   );
 }
 
