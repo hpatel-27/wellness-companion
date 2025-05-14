9d54ebe666263e28791f828b3440d4b7a02b3c70
Harsh <harshpxv@gmail.com>
2025-05-13 21:41:21 -0400

    feat: declaring the routes
    Protect the routes that aren't for login and register and redirect them back to login if not authenticated. Also, defined a path for a not found route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,15 @@ import {
   Route,
   useLocation,
 } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Journal from "./pages/Journal";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./contexts/ProtectedRoute";
+
 import "./styles/index.css";
 
 function AppContent() {
@@ -13,7 +22,40 @@ function AppContent() {
   const isAuthPage =
     location.pathname === "/login" || location.pathname === "/register";
 
-  return <>{!isAuthPage && <NavBar />}</>;
+  return (
+    <>
+      {!isAuthPage && <NavBar />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/journal"
+          element={
+            <ProtectedRoute>
+              <Journal />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 function App() {
