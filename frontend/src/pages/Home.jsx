e49659ba704e823d8eddbd0a91f75f5684438ad7
Harsh <harshpxv@gmail.com>
2025-05-15 16:17:00 -0400

    feat: get user journal and display
    Uses the journal service and passes in auth from the context. The useEffect runs once since it depends on the auth.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import journalService from "../services/journalService";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Home = () => {
+  const [journal, setJournal] = useState(null);
+  const { auth, setAuth } = useContext(AuthContext);
+  useEffect(() => {
+    journalService.getJournal(auth, setAuth).then((data) => {
+      setJournal(data);
+      console.log("HOME RECEIVED: ", data);
+    });
+  }, [auth, setAuth]);
+
+  function printJournal() {
+    console.log("Journal for user: ", journal);
+  }
   return (
     <div>
       <div>Home</div>
+      <button
+        className="flex justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 cursor-pointer"
+        onClick={printJournal}
+      >
+        Click me for goodies
+      </button>
     </div>
   );
 };
