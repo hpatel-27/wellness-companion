30dbd9cf1ee60275925e4370cfac4506da356b06
Harsh <harshpxv@gmail.com>
2025-05-15 16:13:56 -0400

    fix: using the correct url
    Previously the incorrect host was set for the token refresh url. Also, set it in the .env file.

--- frontend/src/util/authFetch.js
+++ frontend/src/util/authFetch.js
@@ -1,8 +1,9 @@
 import { isTokenExpired } from "./jwtUtil";
+const TOKEN_REFRESH = import.meta.env.VITE_TOKEN_REFRESH;
 
 export async function authFetch(url, options = {}, auth, setAuth) {
   if (isTokenExpired(auth.accessToken)) {
-    const refreshResponse = await fetch("/auth/token/refresh/", {
+    const refreshResponse = await fetch(TOKEN_REFRESH, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
