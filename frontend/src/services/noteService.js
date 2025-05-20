2c1a0ab32ccc61703078409d006d0aef1986bdda
Harsh <harshpxv@gmail.com>
2025-05-20 14:32:21 -0400

    feat: service functions to update and get all notes

--- frontend/src/services/noteService.js
+++ frontend/src/services/noteService.js
@@ -3,7 +3,15 @@ import { authFetch } from "../util/authFetch";
 const NOTES_URL = import.meta.env.VITE_NOTES_API_URL;
 const NOTE_URL = import.meta.env.VITE_SINGLE_NOTE_API_URL;
 
-const getAllNotes = () => {};
+const getAllNotes = async (auth, setAuth) => {
+  const response = await authFetch(NOTES_URL, { method: "GET" }, auth, setAuth);
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Error getting notes: ${response.statusText}`);
+  }
+};
 
 const createNote = async (auth, setAuth, noteData) => {
   const response = await authFetch(
@@ -26,4 +34,25 @@ const createNote = async (auth, setAuth, noteData) => {
   }
 };
 
-export default { getAllNotes, createNote };
+const updateNote = async (auth, setAuth, updatedNote) => {
+  const response = await authFetch(
+    NOTE_URL,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedNote),
+    },
+    auth,
+    setAuth
+  );
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Error updating note: ${response.statusText}`);
+  }
+};
+
+export default { getAllNotes, createNote, updateNote };
