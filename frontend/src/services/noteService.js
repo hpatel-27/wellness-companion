482a323e087fd2ea3ad83a574d3ff945f73e6650
Harsh <harshpxv@gmail.com>
2025-05-20 13:45:12 -0400

    feat: sends a request to create a note

--- frontend/src/services/noteService.js
+++ frontend/src/services/noteService.js
@@ -1,6 +1,29 @@
+import { authFetch } from "../util/authFetch";
+
 const NOTES_URL = import.meta.env.VITE_NOTES_API_URL;
 const NOTE_URL = import.meta.env.VITE_SINGLE_NOTE_API_URL;
 
 const getAllNotes = () => {};
 
-export default {};
+const createNote = async (auth, setAuth, noteData) => {
+  const response = await authFetch(
+    NOTES_URL,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(noteData),
+    },
+    auth,
+    setAuth
+  );
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Error creating note: ${response.statusText}`);
+  }
+};
+
+export default { getAllNotes, createNote };
