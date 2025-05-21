91baac67f02a7782ae4a8d7d350771c56430953d
Harsh <harshpxv@gmail.com>
2025-05-20 23:05:16 -0400

    fix: lighter border color on the note

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -17,7 +17,7 @@ const formatDate = (isoString) => {
 const Note = ({ note, handleClick }) => {
   return (
     <div
-      className="border border-gray-400 bg-gray-100 rounded-lg p-4 shadow-md hover:bg-gray-200 transition cursor-pointer"
+      className="border border-gray-200 bg-gray-100 rounded-lg p-4 shadow-md hover:bg-gray-200 transition cursor-pointer"
       onClick={() => handleClick(note.title)}
     >
       <h3 className="text-lg font-medium text-gray-800">{note.title}</h3>
