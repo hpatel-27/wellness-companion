1b8eecc5574953fc300176ebd55e54b97a99b9e2
Harsh <harshpxv@gmail.com>
2025-05-21 00:09:02 -0400

    feat: added delete button for deleting note, updated update button
    Using @heroicon svg's for the create and delete icons. Delete button will submit the form if not explicitly set to type="button". Call the onDelete function when clicked.

--- frontend/src/components/NoteModal.jsx
+++ frontend/src/components/NoteModal.jsx
@@ -166,24 +166,52 @@ const NoteModal = ({ onClose, onSave, onDelete, existingNote = null }) => {
                 ></textarea>
               </div>
             </div>
-            <button
-              type="submit"
-              className="text-white inline-flex items-center bg-blue-500 hover:bg-blue-600 transition focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer"
-            >
-              <svg
-                className="mr-1 -ml-1 w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className="flex justify-between">
+              <button
+                type="submit"
+                className="text-white inline-flex items-center bg-blue-500 hover:bg-blue-600 transition focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center cursor-pointer"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              {existingNote ? "Update Note" : "Create Note"}
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="size-4 mr-1.5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
+
+                {existingNote ? "Update Note" : "Create Note"}
+              </button>
+              {existingNote && (
+                <button
+                  type="button"
+                  onClick={() => onDelete(existingNote?.id)}
+                  className="text-white inline-flex items-center bg-red-500 hover:bg-red-600 transition focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center cursor-pointer"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="size-4 mr-1.5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    />
+                  </svg>
+                  Delete Note
+                </button>
+              )}
+            </div>
           </form>
         </div>
       </div>
