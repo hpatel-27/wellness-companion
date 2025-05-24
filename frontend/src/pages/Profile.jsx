7902136d0254e863606b4ac896081ca2525300f4
Harsh <harshpxv@gmail.com>
2025-05-23 20:22:50 -0400

    feat: profile page initial setup

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleEdit = () => {
+    console.log("Clicked edit button");
+  };
+
   return (
-    <div>
-      <div>Profile</div>
+    <div className="min-h-screen md:h-auto bg-gray-100 px-6 py-8">
+      <div className="max-w-2xl mx-auto pt-15">
+        <section className="bg-white rounded-2xl p-4 ">
+          <div className="flex justify-between items-center pb-4 mb-4 border-b sm:mb-5">
+            <h3 className="text-xl font-semibold text-gray-900">Profile</h3>
+            <button
+              type="button"
+              className="h-8 w-8 rounded-lg bg-blue-400 text-white hover:bg-blue-500 transition flex items-center justify-center cursor-pointer"
+              onClick={handleEdit}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+
+              <span className="sr-only">Edit Profile</span>
+            </button>
+          </div>
+
+          <div className="grid gap-4 mb-4 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                placeholder="John"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                placeholder="Doe"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
