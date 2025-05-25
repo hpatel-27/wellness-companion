52add8180454761503eadc262792f1f682fb7918
Harsh <harshpxv@gmail.com>
2025-05-25 00:37:21 -0400

    fix: updated name for profile url

--- backend/mental_backend/authentication/urls.py
+++ backend/mental_backend/authentication/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("me/", UserView.as_view(), name="current_user"),
+    path("me/", UserView.as_view(), name="user_profile"),
 ]
