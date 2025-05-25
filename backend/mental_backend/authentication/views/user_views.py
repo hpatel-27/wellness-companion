79b213bee5c4ffaa2e9ca4710c72c47727bed3b6
Harsh <harshpxv@gmail.com>
2025-05-25 00:37:39 -0400

    feat: PUT method for updating profile

--- backend/mental_backend/authentication/views/user_views.py
+++ backend/mental_backend/authentication/views/user_views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from ..serializer import UserSerializer
 
+
 # Get the user data for profile information
 class UserView(APIView):
     permission_classes = [IsAuthenticated]
@@ -11,3 +12,10 @@ class UserView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
